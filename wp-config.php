0c49f60b9a5b496ae17d8796b792a4472aa1e7d0
Chung-Yi Chi <cychi1210@gmail.com>
2017-04-30 10:02:46 -0400

    Disable all updates

--- wp-config.php
+++ wp-config.php
@@ -89,3 +89,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+/** Disable all updates.  */
+define('AUTOMATIC_UPDATER_DISABLED', true);
