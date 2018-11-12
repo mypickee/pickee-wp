0d5665070bccb83bc5edfdada1716e463a70dbe1
Chung-Yi Chi <chung-yi@artsymail.com>
2018-11-12 11:20:24 -0500

    Style billing and shipping address on order detail page

--- wp-content/themes/storefront-child/woocommerce/order/order-details-customer.php
+++ wp-content/themes/storefront-child/woocommerce/order/order-details-customer.php
@@ -19,19 +19,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
-$show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_address();
 ?>
 <section class="woocommerce-customer-details">
-
-  <?php if ( $show_shipping ) : ?>
-
   <section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
-    <div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
-
-      <?php endif; ?>
-
-      <h2 class="woocommerce-column__title"><?php _e( 'Billing address', 'woocommerce' ); ?></h2>
-
+    <div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1 order-details-billing-address">
+      <header class="woocommerce-Address-title title">
+        <h2 class="woocommerce-column__title">
+          <?php _e( 'Billing address', 'woocommerce' ); ?>
+        </h2>
+      </header>
       <address>
         <?php echo wp_kses_post( $order->get_formatted_billing_address( __( 'N/A', 'woocommerce' ) ) ); ?>
 
@@ -43,20 +39,17 @@ $show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_a
         <p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
         <?php endif; ?>
       </address>
+    </div>
 
-      <?php if ( $show_shipping ) : ?>
-
-    </div><!-- /.col-1 -->
-
-    <div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
-      <h2 class="woocommerce-column__title"><?php _e( 'Shipping address', 'woocommerce' ); ?></h2>
-        <address>
+    <div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2 order-details-shipping-address">
+      <header class="woocommerce-Address-title title">
+        <h2 class="woocommerce-column__title">
+          <?php _e( 'Shipping address', 'woocommerce' ); ?>
+        </h2>
+      </header>
+      <address>
         <?php echo wp_kses_post( $order->get_formatted_shipping_address( __( 'N/A', 'woocommerce' ) ) ); ?>
-        </address>
-      </div><!-- /.col-2 -->
-
-  </section><!-- /.col2-set -->
-
-  <?php endif; ?>
-
+      </address>
+    </div>
+  </section>
 </section>
