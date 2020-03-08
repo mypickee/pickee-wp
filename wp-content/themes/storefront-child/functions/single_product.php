ed72d7155d28f3a95d0365fe195717707adcf149
Pingchieh <pingchiehchang@gmail.com>
2020-03-08 12:51:23 +0800

    Update single-product page layout and style.

--- wp-content/themes/storefront-child/functions/single_product.php
+++ wp-content/themes/storefront-child/functions/single_product.php
@@ -39,11 +39,11 @@ function single_product_discription()
           <?php if ($attributes) : ?>
           <h4>SPEC</h4>
           <div class="attribute-list">
-            <?php $attributes_count = 1;?>
-            <div class="row clearfix">
+            <!-- <?php $attributes_count = 1;?>
+            <div class="row clearfix"> -->
               <?php foreach ($attributes as $attribute) : ?>
-                <div class="attribute-item <?php if (count($attributes) === 1) {echo 'full-width';}?>">
-                  <strong class="attribute-name"><?php echo wc_attribute_label( $attribute->get_name() ); ?></strong>
+                <div class="attribute-item <?php // if (count($attributes) === 1) {echo 'full-width';}?>">
+                  <span class="attribute-name"><?php echo wc_attribute_label( $attribute->get_name() ); ?></span>
                   <span class="attribute-content">
                     <?php
                       $values = $attribute->get_options();
@@ -55,13 +55,14 @@ function single_product_discription()
                   </span>
                 </div>
                 <?php
-                  if ($attributes_count % 2 === 0) {
+                  /* if ($attributes_count % 2 === 0) {
                     echo '</div><div class="row clearfix">';
                   }
                   $attributes_count++;
+                  */
                 ?>
               <?php endforeach; ?>
-            </div>
+            <!-- </div> -->
           </div>
           <?php endif;?>
           <?php if ($post->post_excerpt) : ?>
@@ -93,16 +94,24 @@ function single_product_reviews()
 
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 
-//Move review rating hearts form "woocommerce_review_before_comment_meta" to "woocommerce_review_meta"
+// Move review rating hearts form "woocommerce_review_before_comment_meta" to "woocommerce_review_meta"
 remove_action('woocommerce_review_before_comment_meta', 'woocommerce_review_display_rating');
 add_action('woocommerce_review_meta', 'woocommerce_review_display_rating', 20);
 
-//remove product data tabs from single-product page.
+// Remove product data tabs from single-product page.
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs');
 
 //Change related products number to 4
 add_filter('woocommerce_output_related_products_args', 'related_products_args', 20);
 function related_products_args($args) {
+  $args['posts_per_page'] = 4;
+  $args['columns'] = 4;
+  return $args;
+}
+
+//Change upsells products number to 4
+add_filter('woocommerce_upsell_display_args', 'upsells_args', 20);
+function upsells_args($args) {
   $args['posts_per_page'] = 4;
   $args['columns'] = 4;
   return $args;
