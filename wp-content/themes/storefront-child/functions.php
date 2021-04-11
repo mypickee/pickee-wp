0e21c79ba7506b342c23170f2879d1700813a13b
Chung-Yi Chi <chung-yi@artsymail.com>
2021-04-11 16:28:44 -0400

    Add Google Analytics

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -6,4 +6,20 @@
 require_once('functions/common.php');
 require_once('functions/single_product.php');
 
+$gid = getenv('GOOGLE_ANALYTICS_ID');
 
+if (!empty($gid)) {
+  add_action('wp_head', function() use ($gid) { add_googleanalytics($gid); });
+
+  function add_googleanalytics($gid) { ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $gid; ?>"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '<?php echo $gid; ?>');
+    </script>
+  <?php }
+}
